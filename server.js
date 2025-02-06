6bd85ba555ca7f2d609ff9db50f5622beea403bf
Vishal Verma <assistantjunior@earninghandle.com>
2025-02-06 10:26:47 +0530

    add limits in uploading

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin : "*"}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // DB connection
 const dbconnect = require('./config/db.js');
