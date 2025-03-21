c9f4e1fb65ce78c1b4e839b194f8b081c6109f65
Vishal Verma <assistantjunior@earninghandle.com>
2025-03-21 10:07:18 +0530

    change validation

--- models/companyLogo/companyLogo.model.js
+++ models/companyLogo/companyLogo.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const companyLogoSchema = new mongoose.Schema({
     image: {
         type: String,
-        required: true,
+        required: false,
     }
 }, { timestamps: true });
 
